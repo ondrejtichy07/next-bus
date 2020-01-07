231232330671fe8a9d25570f1f30aef4aa75a893
Ondrej Tichy <ondrejtichy07@gmail.com>
2020-01-07 12:23:18 +0100

    None request

--- bus.py
+++ bus.py
@@ -10,7 +10,10 @@ URL = "https://pid.cz/zastavkova-tabla/?stop=Hlavn%C3%AD&stanoviste=A"
 
 def get_data():
     r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib')
+    if r != None:
+        soup = BeautifulSoup(r.content, 'html5lib')
+    else:
+        pass
 
     table_body = soup.find('tbody')
     rows = table_body.find_all('tr')
@@ -48,6 +51,7 @@ def get_data():
     print(fg.red + '(%s) ' % data[6][0] + fg.rs + '%s - ' % data[6][1] + fg.green + '%s min' % times[3] + fg.rs)
     print(fg.red + '(%s) ' % data[8][0] + fg.rs + '%s - ' % data[8][1] + fg.green + '%s min' % times[4] + fg.rs)
     print(fg.red + '(%s) ' % data[10][0] + fg.rs + '%s - ' % data[10][1] + fg.green + '%s min' % times[5] + fg.rs)
+    print("========================")
 
 while True:
     get_data()
